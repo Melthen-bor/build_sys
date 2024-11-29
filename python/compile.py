4b084c1eab054fc28c47d09467a18dbe4bc734f6
Malthein Bor <151883748+Melthen-bor@users.noreply.github.com>
2024-11-28 22:44:34 -0800

    Update compile.py

--- python/compile.py
+++ python/compile.py
@@ -1,8 +1,26 @@
 import utils
 import os
 class lang_ext:
-  def __init__(self):
-    self.lang
+  def __init__(self,lang):
+    self.lang=lang
+    self.exts=[]
+  def reg_ext(self,ext):
+    self.exts+=[ext]
+lang_exts=[]
+def register_lang(lang):
+  global lang_exts
+  lang_exts+=[lang_ext(lang)]
+def get_lang(lang):
+  global lang_exts
+  count=0
+  while True:
+    if lang_exts[count].lang==lang:
+      return count
+    count+=1
 def use_gcc(lang,name,gl,libs):
-  raw_sources=os.listdir(os.getcwd()+'\\src')
+  global lang_exts
+  lib_arg=''
+  if libs:
+    lib_arg+=get_file
+  get_file_types(os.listdir(os.getcwd()+'\\src'),lang_exts[get_lang(lang)].exts)
   
